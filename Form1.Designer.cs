8cce1bd11758b5d2f83b321dec0eeecebc8d7fc2
nico <leonardo.cruz1506@gmail.com>
2022-05-29 19:28:52 -0300

    por_2e_3

---
+++ Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿
+namespace por_2e3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txt_nota01 = new System.Windows.Forms.TextBox();
+            this.txt_nota02 = new System.Windows.Forms.TextBox();
+            this.txt_nota03 = new System.Windows.Forms.TextBox();
+            this.txt_nota04 = new System.Windows.Forms.TextBox();
+            this.txt_por2 = new System.Windows.Forms.TextBox();
+            this.txt_por3 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.tb_por2 = new System.Windows.Forms.TextBox();
+            this.tb_por2_2 = new System.Windows.Forms.TextBox();
+            this.tb_por2_3 = new System.Windows.Forms.TextBox();
+            this.tb_por3 = new System.Windows.Forms.TextBox();
+            this.tb_por3_2 = new System.Windows.Forms.TextBox();
+            this.tb_por3_3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(213, 114);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 72);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Calcular";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(335, 34);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Efetuar a leitura de quatro números inteiros e \r\napresentar os números que são di" +
+    "visíveis por 2 e 3.";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Valor01:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "valor02:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 155);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 17);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "valor03:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(7, 188);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(61, 17);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Valor04:";
+            // 
+            // txt_nota01
+            // 
+            this.txt_nota01.Location = new System.Drawing.Point(71, 82);
+            this.txt_nota01.Name = "txt_nota01";
+            this.txt_nota01.Size = new System.Drawing.Size(100, 23);
+            this.txt_nota01.TabIndex = 6;
+            // 
+            // txt_nota02
+            // 
+            this.txt_nota02.Location = new System.Drawing.Point(71, 114);
+            this.txt_nota02.Name = "txt_nota02";
+            this.txt_nota02.Size = new System.Drawing.Size(100, 23);
+            this.txt_nota02.TabIndex = 7;
+            // 
+            // txt_nota03
+            // 
+            this.txt_nota03.Location = new System.Drawing.Point(71, 152);
+            this.txt_nota03.Name = "txt_nota03";
+            this.txt_nota03.Size = new System.Drawing.Size(100, 23);
+            this.txt_nota03.TabIndex = 8;
+            // 
+            // txt_nota04
+            // 
+            this.txt_nota04.Location = new System.Drawing.Point(71, 185);
+            this.txt_nota04.Name = "txt_nota04";
+            this.txt_nota04.Size = new System.Drawing.Size(100, 23);
+            this.txt_nota04.TabIndex = 9;
+            // 
+            // txt_por2
+            // 
+            this.txt_por2.Enabled = false;
+            this.txt_por2.Location = new System.Drawing.Point(15, 272);
+            this.txt_por2.Name = "txt_por2";
+            this.txt_por2.Size = new System.Drawing.Size(114, 23);
+            this.txt_por2.TabIndex = 10;
+            this.txt_por2.WordWrap = false;
+            // 
+            // txt_por3
+            // 
+            this.txt_por3.Enabled = false;
+            this.txt_por3.Location = new System.Drawing.Point(189, 272);
+            this.txt_por3.Name = "txt_por3";
+            this.txt_por3.Size = new System.Drawing.Size(108, 23);
+            this.txt_por3.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(49, 252);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(42, 17);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Por 2";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(219, 252);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(42, 17);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Por 3";
+            // 
+            // tb_por2
+            // 
+            this.tb_por2.Enabled = false;
+            this.tb_por2.Location = new System.Drawing.Point(15, 313);
+            this.tb_por2.Name = "tb_por2";
+            this.tb_por2.Size = new System.Drawing.Size(114, 23);
+            this.tb_por2.TabIndex = 14;
+            this.tb_por2.WordWrap = false;
+            // 
+            // tb_por2_2
+            // 
+            this.tb_por2_2.Enabled = false;
+            this.tb_por2_2.Location = new System.Drawing.Point(15, 354);
+            this.tb_por2_2.Name = "tb_por2_2";
+            this.tb_por2_2.Size = new System.Drawing.Size(114, 23);
+            this.tb_por2_2.TabIndex = 15;
+            this.tb_por2_2.WordWrap = false;
+            // 
+            // tb_por2_3
+            // 
+            this.tb_por2_3.Enabled = false;
+            this.tb_por2_3.Location = new System.Drawing.Point(15, 398);
+            this.tb_por2_3.Name = "tb_por2_3";
+            this.tb_por2_3.Size = new System.Drawing.Size(114, 23);
+            this.tb_por2_3.TabIndex = 16;
+            this.tb_por2_3.WordWrap = false;
+            // 
+            // tb_por3
+            // 
+            this.tb_por3.Enabled = false;
+            this.tb_por3.Location = new System.Drawing.Point(189, 313);
+            this.tb_por3.Name = "tb_por3";
+            this.tb_por3.Size = new System.Drawing.Size(108, 23);
+            this.tb_por3.TabIndex = 17;
+            // 
+            // tb_por3_2
+            // 
+            this.tb_por3_2.Enabled = false;
+            this.tb_por3_2.Location = new System.Drawing.Point(189, 354);
+            this.tb_por3_2.Name = "tb_por3_2";
+            this.tb_por3_2.Size = new System.Drawing.Size(108, 23);
+            this.tb_por3_2.TabIndex = 18;
+            // 
+            // tb_por3_3
+            // 
+            this.tb_por3_3.Enabled = false;
+            this.tb_por3_3.Location = new System.Drawing.Point(189, 398);
+            this.tb_por3_3.Name = "tb_por3_3";
+            this.tb_por3_3.Size = new System.Drawing.Size(108, 23);
+            this.tb_por3_3.TabIndex = 19;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(365, 503);
+            this.Controls.Add(this.tb_por3_3);
+            this.Controls.Add(this.tb_por3_2);
+            this.Controls.Add(this.tb_por3);
+            this.Controls.Add(this.tb_por2_3);
+            this.Controls.Add(this.tb_por2_2);
+            this.Controls.Add(this.tb_por2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txt_por3);
+            this.Controls.Add(this.txt_por2);
+            this.Controls.Add(this.txt_nota04);
+            this.Controls.Add(this.txt_nota03);
+            this.Controls.Add(this.txt_nota02);
+            this.Controls.Add(this.txt_nota01);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txt_nota01;
+        private System.Windows.Forms.TextBox txt_nota02;
+        private System.Windows.Forms.TextBox txt_nota03;
+        private System.Windows.Forms.TextBox txt_nota04;
+        private System.Windows.Forms.TextBox txt_por2;
+        private System.Windows.Forms.TextBox txt_por3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox tb_por2;
+        private System.Windows.Forms.TextBox tb_por2_2;
+        private System.Windows.Forms.TextBox tb_por2_3;
+        private System.Windows.Forms.TextBox tb_por3;
+        private System.Windows.Forms.TextBox tb_por3_2;
+        private System.Windows.Forms.TextBox tb_por3_3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace por_2e3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (txt_nota01.Text == "" || txt_nota02.Text == "" || txt_nota03.Text == "" || txt_nota04.Text == "")
+            {
+
+                MessageBox.Show("PREENCHER TODOS OS CAMPOS", "ATENÇÃO", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+
+            }
+            else
+            {
+                //DECLARANDO E ATRIBUINDO VARIAVEIS
+                int numero01 = Convert.ToInt32(txt_nota01.Text);
+                int numero02 = Convert.ToInt32(txt_nota02.Text);
+                int numero03 = Convert.ToInt32(txt_nota03.Text);
+                int numero04 = Convert.ToInt32(txt_nota04.Text);
+
+                //DIVISIVEIS POR 2
+                if ((numero01 % 2)  == 0)
+                {
+ 
+                    txt_por2.Text = numero01.ToString();
+
+
+
+                }
+                if ((numero02 % 2) == 0)
+                {
+
+                    tb_por2.Text = numero02.ToString();
+
+
+                }
+                if((numero03 % 2) == 0)
+                {
+
+                    tb_por2_2.Text = numero03.ToString();
+
+
+                }
+                if ((numero04 % 2) == 0)
+                {
+
+
+                    tb_por2_3.Text = numero04.ToString();
+
+                }
+
+                //DIVISIVEIS POR 3
+                if((numero01 % 3) == 0)
+                {
+
+                    txt_por3.Text = numero01.ToString();
+     
+
+                }
+                if((numero02 % 3) == 0)
+                {
+
+                    tb_por3.Text = numero02.ToString();
+
+                }
+                if((numero03 % 3) == 0)
+                {
+
+                    tb_por3_2.Text = numero03.ToString();
+                }
+                if((numero04 % 3) == 0)
+                {
+                    tb_por3_3.Text = numero04.ToString();
+
+                }
+         
+
+
+
+            }
+
+            
+        }
+    }
+}
